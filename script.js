95accec5009a6e7fd72cae758197482e65bc1579
sachinsingh94 <sachinjadaun786@gmail.com>
2026-05-16 23:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+// Simple interactivity: highlight project when clicked
+document.querySelectorAll("#projects li").forEach(item => {
+  item.addEventListener("click", () => {
+    item.style.backgroundColor = "#e0f7fa";
+    item.style.cursor = "pointer";
+  });
+});
